b3bc509083ede12efdb5fc5393d3ef9605a1e048
mmeo009 <shh020615@gmail.com>
2023-05-02 17:31:37 +0900

    Player, Cam Moveset and Hand

--- TheWeaponMaster_Garlic/Assets/02.Scripts/AttackCtrl.cs
+++ TheWeaponMaster_Garlic/Assets/02.Scripts/AttackCtrl.cs
@@ -5,28 +5,41 @@ using UnityEngine;
 public class AttackCtrl : MonoBehaviour
 {
     public GameObject Monster;
+    public GameObject hand;
+    public float maxRange;
+    public int weponType = 0;
+    public Ray ray;
+    private void Start()
+    {
+    }
+    private void OnDrawGizmos()
+    {
+
+        Debug.DrawLine(ray.origin, ray.direction * maxRange, Color.red);
+        //화면에 레이를 보여줌
+    }
     void Update()
     {
+        if(weponType == 0)
+        {
+            maxRange = 30.0f;
+        }
         if (Input.GetMouseButtonDown(0))
         {
-            Ray cast = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //화면에서의 2차원 마우스 
-
             RaycastHit hit;
+            ray = new Ray();
+            ray.origin = hand.transform.position;
+            ray.direction = hand.transform.TransformDirection(Vector3.forward);
 
-            if (Physics.Raycast(cast, out hit))
+            if (Physics.Raycast(ray.origin,ray.direction,out hit,maxRange))
             //Rast가 충돌된것을 hit로 돌려준다.
             {
                 if (hit.collider.tag == "Monster")
                 {
                     hit.collider.gameObject.GetComponent<MonsterHp>().GetDmg(1);
                 }
-
                 Debug.Log(hit.collider.name);
                 //오브젝트 이름 출력
-
-                Debug.DrawLine(cast.origin, hit.point, Color.red, 2.0f);
-                //화면에 레이를 보여줌
             }
         }
     }
---
+++ TheWeaponMaster_Garlic/Assets/02.Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform objectFrontVector;
+    private float h = 0.0f;
+    private float v = 0.0f;
+    public float moveSpeed = 10.0f;
+    private float yRotate, yRotateMove;
+    public float rotateSpeed = 500.0f;
+    void Update()
+    {
+        /* player ¿Ãµø */
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        transform.Translate(moveDir.normalized * Time.deltaTime * moveSpeed, Space.Self);
+        yRotateMove = Input.GetAxis("Mouse X") * Time.deltaTime * rotateSpeed;
+
+        yRotate = transform.eulerAngles.y + yRotateMove;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, yRotate, 0);
+    }
+    private void LateUpdate()
+    {
+        Camera.main.transform.position = this.transform.position;
+    }
+}
